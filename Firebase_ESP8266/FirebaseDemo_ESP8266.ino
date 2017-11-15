eb4b52666a8db3b254eaa939e02bf0d22cc8dc1d
Isla <akiyamayami1@gmail.com>
2017-11-15 22:28:42 +0700

    12#

--- Firebase_ESP8266/FirebaseDemo_ESP8266.ino
+++ Firebase_ESP8266/FirebaseDemo_ESP8266.ino
@@ -10,17 +10,22 @@
 
 #define FIREBASE_HOST "testfb-6251e.firebaseio.com"
 #define FIREBASE_AUTH "9Xbvqil0hbtX4MG9QOw5ATlkkEeEceL17AzxzxbM"
-#define WIFI_SSID "TraiTơ"
-#define WIFI_PASSWORD "anhkhongbiet"
+#define WIFI_SSID "qweasd" // Cô đổi thành tên wifi của cô
+#define WIFI_PASSWORD "anhkhongbiet" // đây là pass wifi
+
+//#define WIFI_SSID "xxx"
+//#define WIFI_PASSWORD "12345678"
 
 static void smartdelay(unsigned long ms);
 StaticJsonBuffer<200> jsonBuffer;
 JsonObject& root = jsonBuffer.createObject();
 float lng, lat;
 unsigned long age;
+int proposalID;
 TinyGPS gps;
 SoftwareSerial ss(2, 3);
-
+String address;
+bool flag;
 void setup() {
   Serial.begin(115200);
 
@@ -35,31 +40,41 @@ void setup() {
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
+  proposalID = Firebase.getInt("car/1/proposalID");
 }
 
 char latConvert[32], lngConvert[32];
+int proposal;
 void loop() {
   gps.f_get_position(&lat, &lng, &age);
   dtostrf(lat, 8, 6, latConvert);
   dtostrf(lng, 9, 6, lngConvert);
   root["lat"] = latConvert;
   root["lng"] = lngConvert;
-  root["time"] = 1351824120;
-  Firebase.push("proposal/41", root);
+  address = "proposal/" + String(proposalID);
+  Serial.println(address);
+  Firebase.push(address, root);
   if (Firebase.failed()) {
-    Serial.print("pushing /logs failed:");
     Serial.println(Firebase.error());
     return;
   }
   Serial.println("push");
-  smartdelay(1000);
+  smartdelay(800);
+  proposal = Firebase.getInt("car/1/proposalID");
+  if(proposalID != proposal){
+    Serial.println("Change proposalID:");
+    Serial.print(proposalID + " -> " + proposal);
+    proposalID = proposal;
+  }
+  delay(200);
 }
 static void smartdelay(unsigned long ms) {
   unsigned long start = millis();
   do
   {
     while (ss.available())
+    {
       gps.encode(ss.read());
+    }
   } while (millis() - start < ms);
 }
